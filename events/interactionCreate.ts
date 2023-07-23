a6a063767073824e689fbe61490069f31ed2694b
aeybel <ajqeybel@gmail.com>
2023-07-23 01:21:24 -0400

    add button to event handler

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -3,19 +3,21 @@ import { Events } from 'discord.js';
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
+        if (interaction.isChatInputCommand()) {
+            const command = interaction.client.commands.get(interaction.commandName)
 
-        const command = interaction.client.commands.get(interaction.commandName)
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`)
+            }
 
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`)
-        }
-
-        try {
-            await command.execute(interaction)
-        } catch (error) {
-            console.error(`Error executing ${interaction.commandName}`)
-            console.error(error)
+            try {
+                await command.execute(interaction)
+            } catch (error) {
+                console.error(`Error executing ${interaction.commandName}`)
+                console.error(error)
+            }
+        } else if (interaction.isButton()) {
+            console.log('we clicked a basic button!')
         }
     }
 }
